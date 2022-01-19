9417d91a7fb32c213f2f04936ae1d92fa6d601df
Chris Flemmonds <cflemmonds@gmail.com>
2022-01-19 11:46:47 -0500

    server created with nodemon; express; dotenv

---
+++ app.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+// ! dotenv import must be above all other imports
+const Express = require("express");
+const app = Express()
+
+/*
+    - First we create a variable to import express from node modules folder into out file using require() method.
+    - Creating top level function Express() to gain access to all of its methods and properties:
+        -HTTP requests
+        -middleware
+        -basic app settings
+*/
+
+app.listen(process.env.PORT, () => {
+    console.log(`[server]: app.js is listening on 4000`)
+    // console.log(process.env) // shows all loaded variables that exist in process.env
+})
+
